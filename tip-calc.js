2b88d24332fdbc2a0f43b7f91e49842cb5e1ee39
Renato Guimarães <renatoguidev@gmail.com>
2022-07-22 14:06:15 -0300

    clean js

--- tip-calc.js
+++ tip-calc.js
@@ -3,9 +3,6 @@ let customJS = document.querySelector('#custom')
 let nofJS = document.querySelector('#nof-number')
 let value1JS = document.querySelector('#value1')
 let value2JS = document.querySelector('#value2')
-let alertJS = document.querySelector('#alert')
-
-setInterval(kitten, 1000)
 
 function kitten () {
     var tipsJS = document.querySelector('input[name="tips"]:checked')
@@ -14,8 +11,7 @@ function kitten () {
         let gorget = dolar_numberJS.value * (Number(customJS.value) / 100)
         let valueTotal = Number(dolar_numberJS.value) + Number(gorget)
         value1JS.innerHTML = `$${(gorget / nofJS.value).toFixed(2)}`
-        value2JS.innerHTML = `$${(valueTotal / nofJS.value).toFixed(2)}`
-        console.log(customJS)
+        value2JS.innerHTML = `$${(Number(valueTotal) / nofJS.value).toFixed(2)}`
     } else {
         if (dolar_numberJS.value > 0 && nofJS.value >= 1 && tipsJS.value > 0) {
             let gorget = dolar_numberJS.value * tipsJS.value
@@ -25,6 +21,8 @@ function kitten () {
         }
     }
 }
+
+setInterval(kitten, 1000)
 
 function reset () {
     var tipsJS = document.querySelector('input[name="tips"]:checked')
